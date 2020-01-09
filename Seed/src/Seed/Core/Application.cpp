e09269b6a574a72a92563bc1f0d57fe5c40e5e8a
hightemplarzzyy <hightemplarzzyy@163.com>
2020-01-09 17:18:57 +0800

    LayerStack minor fix

--- Seed/src/Seed/Core/Application.cpp
+++ Seed/src/Seed/Core/Application.cpp
@@ -43,13 +43,11 @@ namespace Seed {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay)
 	{
 		m_LayerStack.PushOverlay(overlay);
-		overlay->OnAttach();
 	}
 
 	void Application::RenderImGui()
